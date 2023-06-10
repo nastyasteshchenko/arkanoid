fe77367bdbbea371d14853d3d4f71b8dd2e2ebbd
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2023-06-10 16:11:17 +0700

    added tests

--- src/test/java/oop/arkanoid/model/BaseLinearEquationTest.java
+++ src/test/java/oop/arkanoid/model/BaseLinearEquationTest.java
@@ -1,5 +1,6 @@
 package oop.arkanoid.model;
 
+import oop.arkanoid.model.barriers.CollisionPlace;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,8 +22,29 @@ class BaseLinearEquationTest {
     }
 
     @Test
-    void getY() {
+    void getYTest() {
         BaseLinearEquation baseLinearEquation = new BaseLinearEquation(45, 15);
         assertEquals(24, baseLinearEquation.getY(9));
+    }
+
+    @Test
+    void rotateByVerticalCollision(){
+        BaseLinearEquation baseLinearEquation = new BaseLinearEquation(45, 15);
+        baseLinearEquation = (BaseLinearEquation) baseLinearEquation.rotate(new Point(15,15), CollisionPlace.RIGHT);
+        assertEquals(Math.tan(Math.toRadians(-225)), baseLinearEquation.k);
+    }
+
+    @Test
+    void rotateByHorizontalCollision(){
+        BaseLinearEquation baseLinearEquation = new BaseLinearEquation(45, 15);
+        baseLinearEquation = (BaseLinearEquation) baseLinearEquation.rotate(new Point(15,15), CollisionPlace.TOP);
+        assertEquals(Math.tan(Math.toRadians(-45)), baseLinearEquation.k);
+    }
+
+    @Test
+    void rotateByPlatform(){
+        BaseLinearEquation baseLinearEquation = new BaseLinearEquation(45, 15);
+        baseLinearEquation = (BaseLinearEquation) baseLinearEquation.rotate(new Point(15,15), 10);
+        assertEquals(Math.tan(Math.toRadians(-100)), baseLinearEquation.k);
     }
 }
