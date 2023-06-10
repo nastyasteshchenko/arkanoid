ae5e60b130b4e4879594489e7cd535d9dc279468
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2023-06-10 17:54:38 +0700

    added tests

--- src/test/java/oop/arkanoid/model/LinearMotionTest.java
+++ src/test/java/oop/arkanoid/model/LinearMotionTest.java
@@ -9,57 +9,55 @@ class LinearMotionTest {
 
     @Test
     void nextPoint() {
-
+        Point nextPoint = createLinearMotion().nextPoint();
+        assertEquals(1, Math.round(nextPoint.x() * 100000) / 100000);
+        assertEquals(1, nextPoint.y());
     }
 
     @Test
     void changeStepIfNeeded() {
-        LinearMotion linearMotion = new LinearMotion(new BaseLinearEquation(45, 15), MotionDirection.RIGHT, 1, new Point(0, 0));
-        linearMotion = linearMotion.changeStepIfNeeded(1.5);
+        LinearMotion linearMotion = createLinearMotion().changeStepIfNeeded(1.5);
         assertEquals(1.5, linearMotion.step);
     }
 
     @Test
     void flipDirection() {
-        LinearMotion linearMotion = new LinearMotion(new BaseLinearEquation(45, 15), MotionDirection.RIGHT, 1, new Point(0, 0));
-        linearMotion = linearMotion.flipDirection();
+        LinearMotion linearMotion = createLinearMotion().flipDirection();
         assertEquals(MotionDirection.LEFT, linearMotion.direction);
     }
 
     @Test
     void flipDirectionByPlatform1() {
-        LinearMotion linearMotion = new LinearMotion(new BaseLinearEquation(45, 15), MotionDirection.RIGHT, 1, new Point(0, 0));
-        linearMotion = linearMotion.flipDirection(-10);
+        LinearMotion linearMotion = createLinearMotion().flipDirection(-10);
         assertEquals(MotionDirection.RIGHT, linearMotion.direction);
     }
 
     @Test
     void flipDirectionByPlatform2() {
-        LinearMotion linearMotion = new LinearMotion(new BaseLinearEquation(45, 15), MotionDirection.RIGHT, 1, new Point(0, 0));
-        linearMotion = linearMotion.flipDirection(10);
+        LinearMotion linearMotion = createLinearMotion().flipDirection(10);
         assertEquals(MotionDirection.LEFT, linearMotion.direction);
     }
 
     @Test
     void rotateByVerticalCollision() {
-        LinearMotion linearMotion = new LinearMotion(new BaseLinearEquation(45, 15), MotionDirection.RIGHT, 1, new Point(0, 0));
-        linearMotion = linearMotion.rotate(CollisionPlace.RIGHT);
+        LinearMotion linearMotion = createLinearMotion().rotate(CollisionPlace.RIGHT);
         assertEquals(Math.tan(Math.toRadians(-225)), linearMotion.linearEquation.k);
 
     }
 
     @Test
     void rotateByHorizontalCollision() {
-        LinearMotion linearMotion = new LinearMotion(new BaseLinearEquation(45, 15), MotionDirection.RIGHT, 1, new Point(0, 0));
-        linearMotion = linearMotion.rotate(CollisionPlace.TOP);
+        LinearMotion linearMotion = createLinearMotion().rotate(CollisionPlace.TOP);
         assertEquals(Math.tan(Math.toRadians(-45)), linearMotion.linearEquation.k);
     }
 
     @Test
     void rotateByPlatform() {
-        LinearMotion linearMotion = new LinearMotion(new BaseLinearEquation(45, 15), MotionDirection.RIGHT, 1, new Point(0, 0));
-        linearMotion = linearMotion.rotate(10);
+        LinearMotion linearMotion = createLinearMotion().rotate(10);
         assertEquals(Math.tan(Math.toRadians(-100)), linearMotion.linearEquation.k);
     }
 
+    private static LinearMotion createLinearMotion() {
+        return new LinearMotion(new BaseLinearEquation(45, BaseLinearEquation.countB(45, new Point(0, 0))), MotionDirection.RIGHT, Math.sqrt(2), new Point(0, 0));
+    }
 }
