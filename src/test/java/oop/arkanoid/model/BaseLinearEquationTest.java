c813ab26db852b48907aae905258fca5b9c3046b
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2023-06-10 15:42:38 +0700

    added tests

--- src/test/java/oop/arkanoid/model/BaseLinearEquationTest.java
+++ src/test/java/oop/arkanoid/model/BaseLinearEquationTest.java
@@ -8,6 +8,16 @@ class BaseLinearEquationTest {
 
     @Test
     void hasIntersection() {
+        BaseLinearEquation baseLinearEquation = new BaseLinearEquation(0, 15);
+        CircleEquation circleEquation = new CircleEquation(new Point(40, 10), 14);
+        assertFalse(baseLinearEquation.getIntersectionPoints(circleEquation).isEmpty());
+    }
+
+    @Test
+    void noIntersection() {
+        BaseLinearEquation baseLinearEquation = new BaseLinearEquation(0, 15);
+        CircleEquation circleEquation = new CircleEquation(new Point(40, 40), 14);
+        assertTrue(baseLinearEquation.getIntersectionPoints(circleEquation).isEmpty());
     }
 
     @Test
