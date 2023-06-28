eec619e5c1c38bdd27cd59d1281f4bc6253f2f42
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2023-06-28 16:01:30 +0700

    created more resizable

--- src/main/java/oop/arkanoid/pane/MainMenuPane.java
+++ src/main/java/oop/arkanoid/pane/MainMenuPane.java
@@ -1,7 +1,11 @@
 package oop.arkanoid.pane;
 
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -9,45 +13,75 @@ import javafx.scene.text.TextAlignment;
 import oop.arkanoid.notifications.EventType;
 import oop.arkanoid.notifications.Notifications;
 
-public class MainMenuPane extends Pane {
-    private final Button startButton = new Button("Start game");
-    private final Button exitButton = new Button("Exit");
-    private final Button aboutButton = new Button("About");
-    private final Button recordsButton = new Button("Records");
-    private final Text arkanoidText = new Text("Arkanoid");
+public class MainMenuPane extends GridPane {
 
     public MainMenuPane() {
-        arkanoidText.setLayoutX(132.0);
-        arkanoidText.setLayoutY(158.0);
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        columnConstraints.setPercentWidth(100.);
+
+
+        RowConstraints textRowConstraints = new RowConstraints();
+        RowConstraints startGameRowConstraints = new RowConstraints();
+        RowConstraints recordsRowConstraints = new RowConstraints();
+        RowConstraints aboutRowConstraints = new RowConstraints();
+        RowConstraints exitRowConstraints = new RowConstraints();
+        RowConstraints spaceRowConstraints = new RowConstraints();
+
+        textRowConstraints.setPercentHeight(30.);
+        startGameRowConstraints.setPercentHeight(10.);
+        recordsRowConstraints.setPercentHeight(10.);
+        aboutRowConstraints.setPercentHeight(10.);
+        exitRowConstraints.setPercentHeight(10.);
+        spaceRowConstraints.setPercentHeight(30.);
+
+        Text arkanoidText = new Text("Arkanoid");
+        GridPane.setConstraints(arkanoidText, 0, 0);
+
+        Button startButton = new Button("Start game");
+        GridPane.setConstraints(startButton, 0, 1);
+
+        Button recordsButton = new Button("Records");
+        GridPane.setConstraints(recordsButton, 0, 2);
+
+        Button aboutButton = new Button("About");
+        GridPane.setConstraints(aboutButton, 0, 3);
+
+        Button exitButton = new Button("Exit");
+        GridPane.setConstraints(exitButton, 0, 4);
+
+        GridPane.setHalignment(arkanoidText, HPos.CENTER);
+        GridPane.setHalignment(startButton, HPos.CENTER);
+        GridPane.setHalignment(recordsButton, HPos.CENTER);
+        GridPane.setHalignment(aboutButton, HPos.CENTER);
+        GridPane.setHalignment(exitButton, HPos.CENTER);
+
+        GridPane.setValignment(arkanoidText, VPos.CENTER);
+        GridPane.setValignment(startButton, VPos.CENTER);
+        GridPane.setValignment(recordsButton, VPos.CENTER);
+        GridPane.setValignment(aboutButton, VPos.CENTER);
+        GridPane.setValignment(exitButton, VPos.CENTER);
+
         arkanoidText.setFont(Font.font("Droid Sans Mono"));
         arkanoidText.setFill(Color.valueOf("RED"));
         arkanoidText.setStyle("-fx-font-size: 70.0");
         arkanoidText.setTextAlignment(TextAlignment.CENTER);
 
         startButton.setOnAction(ae -> Notifications.getInstance().publish(EventType.START_GAME));
-        startButton.setLayoutX(226.0);
-        startButton.setLayoutY(305.0);
         startButton.setPrefSize(150.0, 70.0);
         startButton.setStyle("-fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-font-size: 19.0");
         startButton.setFont(Font.font("Droid Sans Mono"));
 
         aboutButton.setOnAction(ae -> Notifications.getInstance().publish(EventType.ABOUT));
-        aboutButton.setLayoutX(226.0);
-        aboutButton.setLayoutY(513.0);
         aboutButton.setPrefSize(150.0, 70.0);
         aboutButton.setStyle("-fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-font-size: 19.0");
         aboutButton.setFont(Font.font("Droid Sans Mono"));
 
         exitButton.setOnAction(ae -> Notifications.getInstance().publish(EventType.EXIT));
-        exitButton.setLayoutX(226.0);
-        exitButton.setLayoutY(623.0);
         exitButton.setPrefSize(150.0, 70.0);
         exitButton.setStyle("-fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-font-size: 19.0");
         exitButton.setFont(Font.font("Droid Sans Mono"));
 
         recordsButton.setOnAction(ae -> Notifications.getInstance().publish(EventType.RECORDS));
-        recordsButton.setLayoutX(226.0);
-        recordsButton.setLayoutY(409.0);
         recordsButton.setPrefSize(150.0, 70.0);
         recordsButton.setStyle("-fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-font-size: 19.0");
         recordsButton.setFont(Font.font("Droid Sans Mono"));
@@ -57,5 +91,7 @@ public class MainMenuPane extends Pane {
         this.setPrefSize(600, 900);
         this.setOpacity(0.5);
         this.setStyle("-fx-background-color: #FFE4B5");
+        this.getColumnConstraints().add(columnConstraints);
+        this.getRowConstraints().addAll(textRowConstraints, startGameRowConstraints, recordsRowConstraints, aboutRowConstraints, exitRowConstraints, spaceRowConstraints);
     }
 }
