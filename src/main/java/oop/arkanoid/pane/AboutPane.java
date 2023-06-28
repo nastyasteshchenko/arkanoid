fb8ab6043f68dfde7f6429e56ee636ca1e35dd5a
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2023-06-28 16:31:47 +0700

    created more resizable

--- src/main/java/oop/arkanoid/pane/AboutPane.java
+++ src/main/java/oop/arkanoid/pane/AboutPane.java
@@ -1,43 +1,71 @@
 package oop.arkanoid.pane;
 
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import oop.arkanoid.notifications.EventType;
 import oop.arkanoid.notifications.Notifications;
 
-public class AboutPane extends Pane {
-    private final Button backButton = new Button("Back");
-    private final Text aboutText = new Text("The player controls the platform, which prevents a ball from falling from the playing field, and attempts to bounce the ball against a number of bricks. The ball striking a brick causes the brick to disappear. When all the bricks are gone, the player advances to the next level, where another pattern of bricks appears.");
-    private final Text aboutTitleText = new Text("How to play Arkanoid");
+public class AboutPane extends GridPane {
     public AboutPane() {
 
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        columnConstraints.setPercentWidth(100.);
+
+        RowConstraints aboutTitleRowConstraints = new RowConstraints();
+        RowConstraints aboutRowConstraints = new RowConstraints();
+        RowConstraints backRowConstraints = new RowConstraints();
+
+        aboutTitleRowConstraints.setPercentHeight(30.);
+        aboutRowConstraints.setPercentHeight(50.);
+        backRowConstraints.setPercentHeight(20.);
+
+        Label aboutTitleText = new Label("How to play Arkanoid");
+        GridPane.setConstraints(aboutTitleText, 0, 0);
+
+        AnchorPane aboutTextAnchorPane = new AnchorPane();
+        GridPane.setConstraints(aboutTextAnchorPane, 0, 1);
+
+        Button backButton = new Button("Back");
+        GridPane.setConstraints(backButton, 0, 2);
+
+        GridPane.setHalignment(aboutTitleText, HPos.CENTER);
+        GridPane.setHalignment(aboutTextAnchorPane, HPos.CENTER);
+        GridPane.setHalignment(backButton, HPos.CENTER);
+
+        GridPane.setValignment(aboutTitleText, VPos.CENTER);
+        GridPane.setValignment(aboutTextAnchorPane, VPos.TOP);
+        GridPane.setValignment(backButton, VPos.TOP);
+
+        Label aboutText = new Label("The player controls the platform, which prevents a ball from falling from the playing field, and attempts to bounce the ball against a number of bricks. The ball striking a brick causes the brick to disappear. When all the bricks are gone, the player advances to the next level, where another pattern of bricks appears.");
+        AnchorPane.setLeftAnchor(aboutText, 15.);
+        AnchorPane.setRightAnchor(aboutText, 15.);
+
+        aboutTextAnchorPane.getChildren().add(aboutText);
+
         backButton.setOnAction(ae -> Notifications.getInstance().publish(EventType.BACK));
-        backButton.setLayoutX(233.0);
-        backButton.setLayoutY(677.0);
         backButton.setPrefSize(150.0, 70.0);
         backButton.setStyle("-fx-background-color: #ff0000; -fx-text-fill: #ffffff; -fx-font-size: 19.0");
         backButton.setFont(Font.font("Droid Sans Mono"));
 
-        aboutText.setLayoutX(58.0);
-        aboutText.setLayoutY(284.0);
-        aboutText.setWrappingWidth(484);
         aboutText.setStyle("-fx-text-fill: #680707; -fx-text-alignment: CENTER; -fx-font-size: 23.0;");
         aboutText.setFont(Font.font("Droid Sans Mono"));
+        aboutText.setWrapText(true);
 
-
-        aboutTitleText.setLayoutX(84.0);
-        aboutTitleText.setLayoutY(143.0);
-        aboutTitleText.setFill(Color.valueOf("RED"));
+        aboutTitleText.setTextFill(Color.valueOf("RED"));
         aboutTitleText.setStyle("-fx-text-alignment: CENTER; -fx-font-size: 36.0;");
         aboutTitleText.setFont(Font.font("Droid Sans Mono"));
 
-        this.getChildren().addAll(backButton, aboutText, aboutTitleText);
+        this.getChildren().addAll(backButton, aboutTextAnchorPane, aboutTitleText);
         this.setPrefSize(600, 900);
         this.setOpacity(0.5);
         this.setStyle("-fx-background-color: #FFE4B5");
+        this.getColumnConstraints().add(columnConstraints);
+        this.getRowConstraints().addAll(aboutTitleRowConstraints, aboutRowConstraints, backRowConstraints);
     }
 }
 
